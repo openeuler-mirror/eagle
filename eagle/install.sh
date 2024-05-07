96f44d8ca9b4f07c7492f86fd0f6cc50a66f187d
queyanwen <queyanwen@huawei.com>
2024-05-07 09:12:26 +0800

    完善eagle安装目录规划，并改为普通用户eagle运行

--- eagle/install.sh
+++ eagle/install.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
 cd build
-sudo make install 
+sudo make install
+useradd -m eagle
+chown -R eagle /etc/eagle
+
+mkdir /var/log/eagle
+chown -R eagle /var/log/eagle
+
+#Add user "eagle" to POWERAPI list
+PWRAPI_CONF_FILE=/etc/sysconfig/pwrapis/pwrapis_config.ini
+sed -n '/^admin=/p' $PWRAPI_CONF_FILE | grep eagle > /dev/null
+if [ $? -ne 0 ]
+then
+    sed -i '/^admin=/s/$/,eagle/' $PWRAPI_CONF_FILE
+fi
+
 sudo systemctl start eagle.service --now
